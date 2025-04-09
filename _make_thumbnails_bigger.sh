627300f6ea2a15535cbe2675d21c54164a5cf227
k-ashton <kashton@seas.upenn.edu>
2025-04-09 12:47:54 -0400

    Slightly larger images

--- _make_thumbnails_bigger.sh
+++ _make_thumbnails_bigger.sh
@@ -1,3 +1,3 @@
 for file in images/*.{jpg,png}; do
-    [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 440x440 "tn/$file"
+    [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 680x680 "tn/$file"
 done
